f855ce38fb1f1a65ec7835918d78d5d003e97f22
ggautreault <guillaume.gautreault@massmotionmedia.com>
2013-09-17 16:06:50 +0200

    tentative d'ecoute de "HTML5_PLAYER_READY" | "FLASH_PLAYER_READY" pour mettre en pause les specs...

--- src/specs/api_definition.js
+++ src/specs/api_definition.js
@@ -1,44 +1,47 @@
 
+function waitAndTestPlayerReady(){
+	waits( 1000 );
+	setTimeout(	function() { if( ! eventToTest.PLAYER_READY ) waitAndTestPlayerReady();	}, 
+				750 );	
+}
+
 var eventToTest = { "START" : false , "PLAY" : false , "PAUSE" : false , "END" : false , "PLAYER_READY" : false, "NETWORK_UPDATED" : false , "STREAM_ERROR":false } 
 //"CONTROLLER_READY" : false
 
 		
-describe("Le controller est t'il accessible ?", function() {
+describe("Controller is defined ?", function() {
 
-	it("Le controller à été chargé et une instance globale existe (eg. mmm_ACCOUNTNAME) ", function() {
+	it("mmm_ACCOUNTNAME is defined ", function() {
 		runs(function() {
+			
 			expect(mmm_mmm).toBeDefined();
+			
 			window.mmm_ctrl = mmm_mmm;
+
 			for(var i in mmm_ctrlmanager.allControllers){
 				if(i.indexOf("embed_ctrl")!=-1){//l'embed est généré avec un id random
 					window.mmm_ctrl = window[i];
 				}
 			}
-			
-		});
-	});
-	
-	it("Une instance spécifique du controller existe (eg. crée via new mmmController )", function() {
-		runs(function() {
-			
+
 			//dispatché trop tot pour pouvoir le tester ?
 			//window.mmm_ctrl.addEventListener("CONTROLLER_READY", function() { eventToTest.CONTROLLER_READY = true; });
-			
+
 			window.mmm_ctrl.addEventListener("NETWORK_UPDATED", function() { eventToTest.NETWORK_UPDATED = true; });
 			
 			window.mmm_ctrl.addEventListener("START",	function() { eventToTest.START = true; 	});
 			window.mmm_ctrl.addEventListener("PLAY", 	function() { eventToTest.PLAY = true; 	});
 			window.mmm_ctrl.addEventListener("PAUSE", 	function() { eventToTest.PAUSE = true; 	});
 			window.mmm_ctrl.addEventListener("END", 	function() { eventToTest.END = true; 	});
-			
 
-
-			if(mmm_ctrlmanager.device=="computer"){
-				window.mmm_ctrl.addEventListener("FLASH_PLAYER_READY", function() { eventToTest.PLAYER_READY = true; });
-			}else{
-				window.mmm_ctrl.addEventListener("HTML5_PLAYER_READY", function() { eventToTest.PLAYER_READY = true; });
-			}
+			window.mmm_ctrl.addEventListener( (isMobile())? "HTML5_PLAYER_READY" :  "FLASH_PLAYER_READY", function() { eventToTest.PLAYER_READY = true; });
 			
+			
+		});
+	});
+	
+	(window.isEmbed)? it : xit ("embed_ctrl_RANDOM is defined (crée via new mmmController)", function() {
+		runs(function() {
 			expect(window.mmm_ctrl).toBeDefined();			
 		});
 	});
@@ -49,99 +52,99 @@ describe("Le controller est t'il accessible ?", function() {
 // API DEFINE OR NOT DEFINE
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-describe("Les méthodes de l'API (décrites dans la doc) sont elles définie ? ", function() {
+describe("API methode ? ", function() {
 
-	it("mmm_ACCOUNTNAME.play est définie", function() {
+	it("play est définie", function() {
 		runs(function() {
 			expect(window.mmm_ctrl.play).toBeDefined();
 		});
 	});
 	
-	it("mmm_ACCOUNTNAME.pause est définie", function() {
+	it("pause est définie", function() {
 		runs(function() {
 			expect(window.mmm_ctrl.pause).toBeDefined();
 		});
 	});
 	
-	it("mmm_ACCOUNTNAME.playPause est définie", function() {
+	it("playPause est définie", function() {
 		runs(function() {
 			expect(window.mmm_ctrl.playPause).toBeDefined();
 		});
 	});
 	
-	it("mmm_ACCOUNTNAME.replay est définie", function() {
+	it("replay est définie", function() {
 		runs(function() {
 			expect(window.mmm_ctrl.replay).toBeDefined();
 		});
 	});
 	
-	it("mmm_ACCOUNTNAME.rewind est définie", function() {
+	it("rewind est définie", function() {
 		runs(function() {
 			expect(window.mmm_ctrl.rewind).toBeDefined();
 		});
 	});
 	
-	it("mmm_ACCOUNTNAME.seek est définie", function() {
+	it("seek est définie", function() {
 		runs(function() {
 			expect(window.mmm_ctrl.seek).toBeDefined();
 		});
 	});
 	
-	it("mmm_ACCOUNTNAME.mute est définie", function() {
+	it("mute est définie", function() {
 		runs(function() {
 			expect(window.mmm_ctrl.mute).toBeDefined();
 		});
 	});
 	
-	it("mmm_ACCOUNTNAME.soundMute est définie", function() {
+	it("soundMute est définie", function() {
 		runs(function() {
 			expect(window.mmm_ctrl.soundMute).toBeDefined();
 		});
 	});
 	
-	it("mmm_ACCOUNTNAME.volume est définie", function() {
+	it("volume est définie", function() {
 		runs(function() {
 			expect(window.mmm_ctrl.volume).toBeDefined();
 		});
 	});
 	
-	it("mmm_ACCOUNTNAME.getDuration est définie", function() {
+	it("getDuration est définie", function() {
 		runs(function() {
 			expect(window.mmm_ctrl.getDuration).toBeDefined();
 		});
 	});
 
-	it("mmm_ACCOUNTNAME.getCurrentTime est définie", function() {
+	it("getCurrentTime est définie", function() {
 		runs(function() {
 			expect(window.mmm_ctrl.getCurrentTime).toBeDefined();
 		});
 	});
 	
-	it("mmm_ACCOUNTNAME.isPlaying est définie", function() {
+	it("isPlaying est définie", function() {
 		runs(function() {
 			expect(window.mmm_ctrl.isPlaying).toBeDefined();
 		});
 	});
 	
-	it("mmm_ACCOUNTNAME.changeVideo est définie", function() {
+	it("changeVideo est définie", function() {
 		runs(function() {
 			expect(window.mmm_ctrl.changeVideo).toBeDefined();
 		});
 	});
 	
-	it("mmm_ACCOUNTNAME.close est définie", function() {
+	it("close est définie", function() {
 		runs(function() {
 			expect(window.mmm_ctrl.close).toBeDefined();
 		});
 	});
 	
-	it("mmm_ACCOUNTNAME.getMediaPlayer est définie", function() {
+	it("getMediaPlayer est définie", function() {
 		runs(function() {
 			expect(window.mmm_ctrl.getMediaPlayer).toBeDefined();
 		});
 	});
 	
-	it("mmm_ACCOUNTNAME.isFullyLoaded est définie", function() {
+	it("isFullyLoaded est définie", function() {
 		runs(function() {
 			expect(window.mmm_ctrl.isFullyLoaded).toBeDefined();
 		});
@@ -170,6 +173,15 @@ describe("Les méthodes de l'API (décrites dans la doc) sont elles définie ? "
 		runs(function() {
 			expect(mmm_ctrlmanager.currentDevice).toBeDefined();
 		});
+
+		alert("eventToTest.PLAYER_READY " + eventToTest.PLAYER_READY )
+
+		if(!eventToTest.PLAYER_READY ){
+			waitAndTestPlayerReady();
+		}
+
+		
+
 	});
 
 
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,10 @@
+
+//ce fichier doit être est chargé avant les specs js,
+// sans quoi, les méthodeé et variables globales ne serai pas accessible dans les specs js
+
+//variables globales pour prendre des décisions sur l'exécution de telle ou telle specs :
+window.isEmbed = true;
+
 function isMobile(){
 	return screen.width < 500 || navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/iPad/i);
 }
